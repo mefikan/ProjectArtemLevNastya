97804986f4dca25ddcdde5543feabfd6f4a767e0
mefikan <lev.evgenyev@yandex.ru>
2024-04-29 08:34:05 +0300

    Начать делать контроллер

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,14 @@
+class DeviceController
+{
+    async create(req, res) {
+
+    }
+    async get(req, res) {
+
+    }
+    async getOne(req, res) {
+
+    }
+}
+
+module.exports = new TypeController()
